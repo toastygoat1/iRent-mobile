c89fda0f0d07be20ab504822b36f3251c30be348
Michael <johansyahmichael@gmail.com>
2025-05-27 13:44:53 +0700

    tambah desain bottom nav bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,10 +7,33 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Homepage'),
+        title: const Text('Nanti ini gak ada y'),
+        actions : const [
+          Icon(Icons.chat_rounded),
+        ],
       ),
       body: const Center(
         child: Text('Welcome to the Homepage!'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt_rounded),
+            label: 'Transaksi',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Akun',
+          ),
+        ],
+        currentIndex: 0,
+        onTap: (index) {
+          // Handle navigation here
+        },
       ),
     );
   }
