b0954d542fa901d6b0d7f7fa760a2e03abc55cd9
HafizD <hafizdicky111@gmail.com>
2025-05-27 22:38:21 +0700

    Mentahan

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -6,208 +6,315 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: CustomAppBar(),
-        body: ProductPage(),
+      title: 'iPhone Product Detail',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Roboto',
       ),
+      home: ProductDetailPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class ProductPage extends StatefulWidget {
-  @override
-  _ProductPageState createState() => _ProductPageState();
-}
-
-class _ProductPageState extends State<ProductPage> {
-  bool isLoved = false;
-
+class ProductDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Gambar + ikon love
-          Stack(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 500,
-                child: Image.asset(
-                  'assets/images/15.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Positioned(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // App Bar
+            Container(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).padding.top + 10,
+                left: 16,
+                right: 16,
                 bottom: 10,
-                right: 20,
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      isLoved = !isLoved;
-                    });
-                  },
-                  child: Icon(
-                    isLoved ? Icons.favorite : Icons.favorite_border,
-                    color: isLoved ? Colors.red : Colors.white,
-                    size: 45,
-                  ),
-                ),
               ),
-            ],
-          ),
-
-          // Spasi kecil
-          SizedBox(height: 12),
-
-          // Harga
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              'Rp.499.000',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-          ),
-
-          // Nama Produk & Rating
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'iPhone 15 pro max RAM 32GB',
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 16,
-                  ),
-                ),
-                SizedBox(height: 6),
-                Row(
-                  children: [
-                    Icon(Icons.star, color: Colors.amber, size: 18),
-                    SizedBox(width: 4),
-                    Text(
-                      '5.0 ',
-                      style: TextStyle(fontSize: 14, color: Colors.black),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: Icon(
+                      Icons.arrow_back,
+                      size: 24,
+                      color: Colors.black,
                     ),
-                    Text(
-                      '(114)',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.black.withOpacity(0.6),
-                        decoration: TextDecoration.underline,
+                  ),
+                  Spacer(),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.chat_bubble_outline,
+                            size: 16,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 4),
+                          Text(
+                            'RP',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
-          ),
 
-          // Garis pembatas horizontal
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            child: Container(
-              height: 1,
+            // Product Images
+            Container(
+              height: 300,
+              child: PageView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'assets/iphone_colors.png', // Placeholder
+                        height: 250,
+                        fit: BoxFit.contain,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.phone_iphone,
+                                size: 80,
+                                color: Colors.grey[400],
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'iPhone 15 128GB\n256GB 512GB',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  _buildColorCircle(Colors.black),
+                                  _buildColorCircle(Colors.white),
+                                  _buildColorCircle(Colors.blue[100]!),
+                                  _buildColorCircle(Colors.yellow[100]!),
+                                  _buildColorCircle(Colors.pink[100]!),
+                                ],
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            // Price Section
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Rp 499.000',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'iPhone 15 pro max RAM 32GB',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.amber, size: 16),
+                      SizedBox(width: 4),
+                      Text(
+                        '5.0',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        '(114)',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 30),
+
+            // Detail Produk Section
+            Container(
               width: double.infinity,
-              color: Color(0xFF979797),
-            ),
-          ),
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Detail produk',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 20),
 
-          // Deskripsi produk
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Deskripsi produk',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+                  _buildDetailRow('Status Sinyal', 'Sinyal Aktif'),
+                  _buildDetailRow('Tahun Rilis', '2023'),
+                  _buildDetailRow('Tipe Garansi', 'Garansi Distributor'),
+                  _buildDetailRow('Etalase', 'New FS', valueColor: Colors.green),
+
+                  SizedBox(height: 30),
+
+                  Text(
+                    'Deskripsi produk',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'Seluruh produk yang kami jual merupakan 100% original, '
-                      '100% baru, 100% garansi resmi dan kepuasan Anda adalah prioritas kami.',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.black87,
+                  SizedBox(height: 15),
+
+                  Text(
+                    'Seluruh produk yang kami jual merupakan 100% original, 100% baru, 100% garansi resmi dan kepuasan pelanggan adalah prioritas utama kami.',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.black87,
+                      height: 1.5,
+                    ),
                   ),
-                ),
-              ],
+
+                  SizedBox(height: 15),
+
+                  Text(
+                    'Link Aksesoris iPhone : https://www.tokopedia.com/studioponsel/etalase/iphone-accessories',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.black87,
+                      height: 1.5,
+                    ),
+                  ),
+
+                  SizedBox(height: 10),
+
+                  Text(
+                    'Review pelanggan STUDIO PONSEL : https://www.tokopedia.com/studioponsel/review',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.black87,
+                      height: 1.5,
+                    ),
+                  ),
+
+                  SizedBox(height: 15),
+
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      'Baca Selengkapnya',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.green,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+
+            SizedBox(height: 100), // Space for bottom navigation
+          ],
+        ),
       ),
     );
   }
-}
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Color(0xFFFFF5F5),
-      elevation: 0,
-      titleSpacing: 0,
-      leading: IconButton(
-        icon: Icon(Icons.arrow_back, color: Colors.black),
-        onPressed: () {
-          Navigator.pop(context);
-        },
+  Widget _buildColorCircle(Color color) {
+    return Container(
+      width: 20,
+      height: 20,
+      margin: EdgeInsets.symmetric(horizontal: 3),
+      decoration: BoxDecoration(
+        color: color,
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Colors.grey[300]!,
+          width: 1,
+        ),
       ),
-      title: Row(
+    );
+  }
+
+  Widget _buildDetailRow(String label, String value, {Color? valueColor}) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 15),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            'Back',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 15,
+          SizedBox(
+            width: 120,
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[600],
+              ),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Container(
-                height: 40,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.blue),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 12),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Tulis sesuatu...",
-                    border: InputBorder.none,
-                  ),
-                ),
+            child: Text(
+              value,
+              style: TextStyle(
+                fontSize: 14,
+                color: valueColor ?? Colors.black87,
+                fontWeight: valueColor != null ? FontWeight.w500 : FontWeight.normal,
               ),
             ),
-          ),
-          IconButton(
-            icon: Icon(Icons.search, color: Colors.black),
-            onPressed: () {
-              // Aksi pencarian
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.chat_bubble_outline, color: Colors.black),
-            onPressed: () {
-              // Aksi untuk membuka chat
-            },
           ),
         ],
       ),
